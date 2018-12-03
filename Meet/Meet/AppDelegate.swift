81cfceb32a37062402516b821435c5d5fece325c
PWDream <1184734081@qq.com>
2018-12-03 22:06:32 +0800

    引入高德API

--- Meet/Meet/AppDelegate.swift
+++ Meet/Meet/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         UITabBar.appearance().tintColor = RGBCOLOR_HEX(h: 0xd81e06)
         UINavigationBar.appearance().tintColor = UIColor.gray
+        AMapServices.shared().apiKey = GAODEKEY
         return true
     }
 
---
+++ Meet/Meet/Utilities/APIKey.swift
@@ -0,0 +1,10 @@
+//
+//  APIKey.swift
+//  Meet
+//
+//  Created by 潘威 on 2018/12/3.
+//  Copyright © 2018 潘威. All rights reserved.
+//
+
+import Foundation
+public let GAODEKEY = "98ac6a0d1b7ee15d43b353a55d3ad0d7"
